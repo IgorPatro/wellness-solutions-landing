96dcbacfc9c01d6d3ace7579f60a1f9bf6185b65
Igor Patro <i.patro@wp.pl>
2024-09-15 17:14:53 +0200

    darked colors

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -8,16 +8,16 @@ module.exports = {
         secondaryColor: "rgb(161, 163, 247)",
         primaryText: "rgb(255,255,255)",
         secondaryText: "rgb(174, 178, 183)",
-        bgDark1: "rgb(31, 32, 35)",
-        bgDark2: "rgb(38, 39, 43)",
-        bgDark3: "rgb(48, 49, 54)",
+        bgDark1: "rgb(22, 23, 26)",
+        bgDark2: "rgb(29, 30, 34)",
+        bgDark3: "rgb(39, 40, 45)",
         bgDark3Hover: "rgb(55, 56, 62)",
         bgDarkTransparent: "rgb(31, 32, 35, 0.7)",
         bgDarkTransparentDarker: "rgb(0,0,0,0.5)",
         bgDarkTransparentLighter: "rgb(48, 49, 54, 0.7)",
         mainBorder: "rgb(255,255,255,0.15)",
         mainBorderDarker: "rgb(255,255,255,0.07)",
-        quoteIconColor: "rgb(178, 184, 205)"
+        quoteIconColor: "rgb(178, 184, 205)",
       },
       fontFamily: {
         Inter: "Inter",
@@ -27,10 +27,9 @@ module.exports = {
         sm: "640px",
         md: "768px",
         lg: "1024px",
-        xl: "1280px",      
+        xl: "1280px",
         "2xl": "1536px",
       },
     },
   },
 };
-
